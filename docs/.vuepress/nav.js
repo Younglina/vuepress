5aa347b5a1243217400895427ae3c1736e652254
Younglina <Younglina@Outlook.com>
2022-07-12 23:05:46 +0800

    feat: nav

--- docs/.vuepress/nav.js
+++ docs/.vuepress/nav.js
@@ -347,6 +347,11 @@ module.exports = [
                 link: "/write/algorithms/leetCode347",
             },
             {
+                text: "873.最长的斐波那契子序列的长度",
+                title: "873.最长的斐波那契子序列的长度",
+                link: "/write/algorithms/leetCode873",
+            },
+            {
                 text: "剑指Offer26.树的子结构",
                 link: "/write/algorithms/leetCode_o26.md",
             },
